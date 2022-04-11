c75f00010329a013f6cbe69d2665e259b4e06f84
XKBAY <74855263+XKBAY@users.noreply.github.com>
2022-04-11 20:38:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,19 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+git checkout 1a86fc37114da97764e337908d05e0752941acb9
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+
+echo '添加jerrykuku的argon-mod主题'
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+echo '=========Add argon-mod OK!========='
